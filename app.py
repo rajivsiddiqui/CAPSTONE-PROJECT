26d793f9bdb3212418f5de91d015c83e1416c069
SIVAKUMAR-PC\SIVAKUMAR <cloudarchitectaws@gmail.com>
2021-09-05 14:19:21 +0530

    Edited app.y

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome, My name is Sivakumar kS , This is Udacity final project Blue Deployment version 1 before rolling update"
+    return "Welcome, My name is Sivakumar kS , This is Udacity final project Blue Deployment Version 1 before rolling update"
 app.run(host="0.0.0.0", port=8080, debug=True)
